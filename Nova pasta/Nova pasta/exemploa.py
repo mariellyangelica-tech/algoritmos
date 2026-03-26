80e91be98527108914b37b34d1785dbf30ab1ce3
mariellyangelica-tech <mariellyangelica77@gmail.com>
2026-03-26 19:58:17 -0400

    Add files via upload

---
+++ Nova pasta/Nova pasta/exemploa.py
@@ -0,0 +1,26 @@
+print("hello world") 
+
+valor1 = 3
+
+# é para comentarios 
+
+# ctrl +; tamben funciona
+"""
+exemplo
+"""
+
+nome = "mary" #isso é uma string, ou seja caracteres
+
+#toda variavel pode ter letras e numeros, ma nunca começar com um numero 
+#não pode ter caractere especial 
+#não pode ter espaço no nome 
+#nome_completo = nome composto deveter underline
+#deve ser significativa
+#case-sensiitive = diferencia maiusculo e min
+
+numero1 = 2
+numero2 = 3
+
+resultado = numero1 + numero2
+
+print(resultado)
